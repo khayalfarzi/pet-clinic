fd1d3b163f31346da3e1c5837bab8672ad049c8c
khayalfarzi <xeyalferziyev9@gmail.com>
2020-01-01 10:59:53 +0300

    create models

---
+++ src/main/java/com/springpetclinic/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springpetclinic.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springpetclinic/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.springpetclinic.petclinic.model;
+
+public class Vet extends Person {
+
+}
